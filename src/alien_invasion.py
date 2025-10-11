05759db0789e76fa3fb5d14dd121ebd7b3eee8e1
Winter4Life <taylorak1201@protonmail.com>
2025-10-11 14:51:42 -0700

    changing movement keys

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -42,16 +42,16 @@ class AlienInvasion:
                         
     def _check_keydown_events(self, event):
         """Response to keypresses"""
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_d:
             self.ship.moving_right = True
-        elif  event.key == pygame.K_LEFT:
+        elif  event.key == pygame.K_a:
             self.ship.moving_left = True
         
     def _check_keyup_events(self, event):
         """Response to keyreleases"""
-        if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_d:
             self.ship.moving_right = False
-        elif  event.key == pygame.K_LEFT:
+        elif  event.key == pygame.K_a:
             self.ship.moving_left = False
                     
     def _update_screen(self):
